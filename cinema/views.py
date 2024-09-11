6261adad3bfd382645290de68ebedf0b1639577c
Anton Bilous <anton.bilosus@gmail.com>
2024-09-11 17:20:34 +0300

    Create GenreViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,9 @@
-# write views here
+from rest_framework import viewsets
+
+from .models import Genre
+from .serializers import GenreSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
