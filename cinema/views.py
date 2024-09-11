2bebea78c5e8414cfd5742a34701af953d0ede08
Anton Bilous <anton.bilosus@gmail.com>
2024-09-11 17:28:54 +0300

    Create ActorViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from .models import Genre
-from .serializers import GenreSerializer
+from .models import Genre, Actor
+from .serializers import GenreSerializer, ActorSerializer
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
