254d390710867d9c836b35046bdc3adf07ba33cc
Anton Bilous <anton.bilosus@gmail.com>
2024-09-11 17:20:10 +0300

    Create GenreSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,9 @@
-# write serializers here
+from rest_framework import serializers
+
+from .models import Genre
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
