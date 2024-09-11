88a4139685c52643e9658aa496bd3bb89e5e2a4c
Anton Bilous <anton.bilosus@gmail.com>
2024-09-11 17:21:03 +0300

    Populate cinema/urls.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,14 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import GenreViewSet
+
+
+app_name = "cinema"
+
+router = routers.SimpleRouter()
+router.register("genres", GenreViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
