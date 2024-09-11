7c0173a39e51730357bc4d8b496ac87d495cfaf5
Anton Bilous <anton.bilosus@gmail.com>
2024-09-11 17:44:18 +0300

    Add MovieSessionViewSet to router

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import GenreViewSet, ActorViewSet, CinemaHallViewSet, MovieViewSet
+from .views import (
+    GenreViewSet,
+    ActorViewSet,
+    CinemaHallViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+)
 
 
 app_name = "cinema"
@@ -11,6 +17,7 @@ router.register("genres", GenreViewSet)
 router.register("actors", ActorViewSet)
 router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
+router.register("movie_sessions", MovieSessionViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
