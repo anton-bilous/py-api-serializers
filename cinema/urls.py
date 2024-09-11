9be7ccaf580cc3cfbc549e3af2c63814a3c1552c
Anton Bilous <anton.bilosus@gmail.com>
2024-09-11 17:29:14 +0300

    Add ActorViewSet to router

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import GenreViewSet
+from .views import GenreViewSet, ActorViewSet
 
 
 app_name = "cinema"
 
 router = routers.SimpleRouter()
 router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
