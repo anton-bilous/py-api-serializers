c6dec1632aebb59cd35777231f005bd1383a34dc
Anton Bilous <anton.bilosus@gmail.com>
2024-09-11 17:28:22 +0300

    Create ActorSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import Genre
+from .models import Genre, Actor
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
+        fields = "__all__"
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
         fields = "__all__"
